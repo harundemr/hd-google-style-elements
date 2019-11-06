98728253512e3e19605cf6bb39cd47dfae02bd9b
Harun Demir <harundemirp@gmail.com>
2019-11-06 15:49:29 +0300

    textarea complated

--- js/hd-script.js
+++ js/hd-script.js
@@ -1,21 +1,53 @@
 
 function hdInit(){
-    var inputDivList = document.querySelectorAll(".hd-input-text-div");
-    var inputElementList = document.querySelectorAll(".hd-input-text");
 
-    for(var i = 0; i < inputDivList.length; i++)
+    /*text*/
+    var inputTextDivList = document.querySelectorAll(".hd-input-text-div");
+    var inputTextElementList = document.querySelectorAll(".hd-input-text");
+
+    for(var i = 0; i < inputTextDivList.length; i++)
     {
-        inputDivList[i].setAttribute("onclick", "hdFocusIn(this)");
-        inputDivList[i].setAttribute("onfocusout", "hdFocusOut(this)");
+        inputTextDivList[i].setAttribute("onclick", "hdTextFocusIn(this)");
+        inputTextDivList[i].setAttribute("onfocusout", "hdTextFocusOut(this)");
     }
 
-    for(var i = 0; i < inputElementList.length; i++)
+    for(var i = 0; i < inputTextElementList.length; i++)
     {
-        inputElementList[i].setAttribute("onfocusin", "hdFocusIn(this.parentElement)");
+        inputTextElementList[i].setAttribute("onfocusin", "hdTextFocusIn(this.parentElement)");
+    }
+
+    /*TextArea*/
+    var inputTextAreaDivList = document.querySelectorAll(".hd-input-textarea-div");
+    var inputTextAreaElementList = document.querySelectorAll(".hd-input-textarea");
+
+    for(var i = 0; i < inputTextAreaDivList.length; i++)
+    {
+        inputTextAreaDivList[i].setAttribute("onclick", "hdTextAreaFocusIn(this)");
+        inputTextAreaDivList[i].setAttribute("onfocusout", "hdTextAreaFocusOut(this)");
+    }
+
+    for(var i = 0; i < inputTextAreaElementList.length; i++)
+    {
+        inputTextAreaElementList[i].setAttribute("onfocusin", "hdTextAreaFocusIn(this.parentElement)");
+    }
+
+    /*Select*/
+    var selectDivList = document.querySelectorAll(".hd-input-textarea-div");
+    var selectElementList = document.querySelectorAll(".hd-input-textarea");
+
+    for(var i = 0; i < selectDivList.length; i++)
+    {
+        selectDivList[i].setAttribute("onclick", "hdTextAreaFocusIn(this)");
+        selectDivList[i].setAttribute("onfocusout", "hdTextAreaFocusOut(this)");
+    }
+
+    for(var i = 0; i < selectElementList.length; i++)
+    {
+        selectElementList[i].setAttribute("onfocusin", "hdTextAreaFocusIn(this.parentElement)");
     }
 }
 
-function hdFocusIn(e){
+function hdTextFocusIn(e){
     input = e.querySelector(".hd-input-text");
     placeholder = e.querySelector(".hd-input-text-placeholder");
 
@@ -28,7 +60,7 @@ function hdFocusIn(e){
 
 }
 
-function hdFocusOut(e){
+function hdTextFocusOut(e){
     input = e.querySelector(".hd-input-text");
     placeholder = e.querySelector(".hd-input-text-placeholder");
 
@@ -43,4 +75,31 @@ function hdFocusOut(e){
     input.focusout();
 }
 
+function hdTextAreaFocusIn(e){
+    input = e.querySelector(".hd-input-textarea");
+    placeholder = e.querySelector(".hd-input-textarea-placeholder");
+
+    placeholder.style.animationName = "yukari";
+    placeholder.style.animationDuration = ".15s";
+    placeholder.style.animationFillMode = "forwards";
+    placeholder.style.color = "#1a73e8";
+
+    input.focus();
+
+}
+
+function hdTextAreaFocusOut(e){
+    input = e.querySelector(".hd-input-textarea");
+    placeholder = e.querySelector(".hd-input-textarea-placeholder");
+
+    if(input.value == "")
+    {
+        placeholder.style.animationName = "asagi";
+        placeholder.style.animationDuration = ".15s";
+        placeholder.style.animationFillMode = "forwards";
+    }
+    placeholder.style.color = "#80868b";
+
+    input.focusout();
+}
 hdInit();
